3f34c5f7ae10596143ac4b347a2881a4c4302f68
Hazhar Maroufi <hazhmar@gmail.com>
2024-11-19 09:23:43 +0330

    style

--- src/CourseCard.jsx
+++ src/CourseCard.jsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
+import "./CourseCard.css";
+
 function CourseCard({ data: { name, desc } }) {
+  const [selected, setSelected] = useState(false);
+  const selectHandler = () => {
+    setSelected((selected) => !selected);
+  };
   return (
-    <li>
+    <li className={`cardList ${selected ? "select" : ""}`}>
       <h4>{name}</h4>
       <p>{desc}</p>
+      <button onClick={selectHandler}>Select</button>
     </li>
   );
 }
