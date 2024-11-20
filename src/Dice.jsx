2592fc331c000de2d0deba116af6f47101f75247
Hazhar Maroufi <hazhmar@gmail.com>
2024-11-20 11:59:28 +0330

    end of this project

--- src/Dice.jsx
+++ src/Dice.jsx
@@ -1,13 +1,23 @@
+import { useState } from "react";
 import styled from "styled-components";
 const Span = styled.span`
-  font-size: 24px;
+  font-size: ${(props) => (props.clicked ? "24px" : "16px")};
   color: blue;
 `;
 
 function Dice() {
+  const [clicked, setClicked] = useState(true);
+
   return (
     <>
-      <Span>You got the number </Span>
+      <button
+        onClick={() => {
+          setClicked((s) => !s);
+        }}
+      >
+        size
+      </button>
+      <Span clicked={clicked}>You got the number </Span>
       <Span>{Math.floor(Math.random() * 6) + 1}</Span>
     </>
   );
